1ef69085b1dd0adda576f4b5836e71f313bc892b
Alexander Dionicio <alexander.dionicio@Falconeye.local>
2020-03-10 14:19:15 -0400

    views

--- src/core/views.py
+++ src/core/views.py
@@ -3,12 +3,28 @@ from django.http import JsonResponse
 
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import mixins
 
 from .Serializer import PostSerializer
 from .models import Post
 
 
+class PostView(mixins.ListModelMixin,
+               mixins.CreateModelMixin,
+               generics.GenericAPIView):
+
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(self, request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+
+"""
 # Create your views here.
 class TestView(APIView):
 
@@ -27,3 +43,5 @@ class TestView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+    
+    """
--- src/drf_api/urls.py
+++ src/drf_api/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import TestView
+from core.views import PostView
 from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',TestView.as_view(), name="test"),
+    path('', PostView.as_view(), name="test"),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', obtain_auth_token, name="obtain-token" ),
     path('rest-auth/', include('rest_auth.urls'))
